406d329f391a4fea0ece7417849052bc79a5fd99
Kevin Xiao <xlzqwerty1@gmail.com>
2026-02-21 11:29:11 -0800

    Add daily claim status badge to UI (#33)

--- frontend/src/modules/games/hsr/components/HsrSection.tsx
+++ frontend/src/modules/games/hsr/components/HsrSection.tsx
@@ -4,18 +4,21 @@ import {
   getResourceDisplayName,
   HsrResource,
 } from "@/modules/games/games.constants";
+import { GameId } from "@/modules/games/games.types";
 import { StaminaCard } from "@/modules/resources/components/StaminaCard";
 import { GameSection } from "@/modules/ui/components/GameSection";
 import * as m from "@/paraglide/messages";
 
 export const HsrSection: React.FC = () => {
   const isRefreshing = useAtomValue(atoms.core.isRefreshing);
+  const claimStatusMap = useAtomValue(atoms.core.dailyClaimStatus);
+  const claimStatus = claimStatusMap.get(GameId.HonkaiStarRail) ?? null;
 
   const tbpData = useAtomValue(atoms.games.hsr.trailblazePower);
   const tbpTime = useAtomValue(atoms.games.hsr.trailblazePowerTime);
 
   return (
-    <GameSection title={m.game_hsr_name()}>
+    <GameSection title={m.game_hsr_name()} claimStatus={claimStatus}>
       <StaminaCard
         iconPath="/icons/game/hsr/Item_Trailblaze_Power.webp"
         name={getResourceDisplayName(HsrResource.TrailblazePower)}
--- frontend/src/modules/games/zzz/components/ZzzSection.tsx
+++ frontend/src/modules/games/zzz/components/ZzzSection.tsx
@@ -4,18 +4,21 @@ import {
   getResourceDisplayName,
   ZzzResource,
 } from "@/modules/games/games.constants";
+import { GameId } from "@/modules/games/games.types";
 import { StaminaCard } from "@/modules/resources/components/StaminaCard";
 import { GameSection } from "@/modules/ui/components/GameSection";
 import * as m from "@/paraglide/messages";
 
 export const ZzzSection: React.FC = () => {
   const isRefreshing = useAtomValue(atoms.core.isRefreshing);
+  const claimStatusMap = useAtomValue(atoms.core.dailyClaimStatus);
+  const claimStatus = claimStatusMap.get(GameId.ZenlessZoneZero) ?? null;
 
   const batteryData = useAtomValue(atoms.games.zzz.battery);
   const batteryTime = useAtomValue(atoms.games.zzz.batteryTime);
 
   return (
-    <GameSection title={m.game_zzz_name()}>
+    <GameSection title={m.game_zzz_name()} claimStatus={claimStatus}>
       <StaminaCard
         iconPath="/icons/game/zzz/Item_Battery_Charge.webp"
         name={getResourceDisplayName(ZzzResource.Battery)}
